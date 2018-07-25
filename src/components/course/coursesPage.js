d4472d7e220aa8adfa138fdfb6754617b71775d6
Varun Mehta <mehtav23@gmail.com>
2018-07-25 08:30:06 +0530

    added store and reducers

---
+++ src/components/course/coursesPage.js
@@ -0,0 +1,65 @@
+import React, { PropTypes } from 'react';
+import {connect} from 'react-redux';
+import * as courseAction from '../../actions/courseActions';
+
+class CoursesPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            course: { title: '' }
+        };
+
+        this.onClickSave = this.onClickSave.bind(this);
+        this.onTitleChange = this.onTitleChange.bind(this);
+        
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+
+    onClickSave() {
+        this.props.dispatch(courseAction.createCourse(this.state.course));
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
+                <h2>Add Course</h2>
+
+                <input
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title} />
+
+                <input
+                    type="submit"
+                    onClick={this.onClickSave}
+                    value="Save" />
+            </div>
+        );
+    }
+}
+
+CoursesPage.PropTypes = {
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps (state, ownProps) {
+    return {
+        courses: state.courses
+    };
+
+}
+
+export default connect(mapStateToProps)(CoursesPage);
